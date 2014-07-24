a336006d8cfd401014c4e60f3d3ec86aff8eb2c4
Hypolymer <jonesw@geneseo.edu>
2014-07-24 15:09:29 -0400

    Create PopupMessage.lua

---
+++ PopupMessage.lua
@@ -0,0 +1,6 @@
+-- Include this line within a function to display informaiton in a text box
+-- The first line in quotes is the message to the user.
+-- The second line in quotes is the name/heading of the MessageBox
+
+
+interfaceMngr:ShowMessage("ISxN is not available from request form", "Insufficient Information");
