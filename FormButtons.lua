1206ea3e08a0e22522a54b7f05b08d38fd3e3bac
Hypolymer <jonesw@geneseo.edu>
2014-07-27 19:37:14 -0400

    Update FormButtons.lua

--- FormButtons.lua
+++ FormButtons.lua
@@ -1,3 +1,16 @@
---Add text
+-- The two methods below show ways to submit the value within a search form.
 
-SubmitForm(string formName)
+-- One way is to reference the name of the form that is being submitted.
+-- Example:
+
+AddonNameForm.Browser:SubmitForm(string formName);
+
+-- For Google, this would be:
+SomeGoogleAddon.Browser:SubmitForm("gbqf");
+
+
+-- You can also reference the name of a button.
+AddonNameForm.Browser:ClickObject(string formName)
+
+-- For ExLibris Primo, this would be:
+ExLibrisForm.Browser:ClickObject("goButton")
