4cc427be4edec3d66fbd2b7ac611c8805b7b4a90
Hypolymer <jonesw@geneseo.edu>
2014-07-27 19:52:36 -0400

    Update GetFieldValue.lua

--- GetFieldValue.lua
+++ GetFieldValue.lua
@@ -1,1 +1,29 @@
--- Add text
+-- There are many fields in the request that you will want to get information from for your Addon.
+-- The table the information is pulled from is usually the Transactions Table.
+-- For more information about fields within the Transactions Table, visit:  
+-- https://prometheus.atlas-sys.com/display/illiad/Key+ILLiad+Data+Tables
+
+-- To get the value of a field within the table, use the following command:
+GetFieldValue("Name of Table", "Name of Field");
+
+
+-- Below are common fields used to get data from.
+
+-- LoanAuthor (Author for a returnable item)
+GetFieldValue("Transaction", "LoanAuthor");
+
+-- LoanTitle (Title for a returnable item)
+GetFieldValue("Transaction", "LoanTitle");
+
+-- ArticleAuthor (Author for an article)
+GetFieldValue("Transaction", "PhotoArticleAuthor");
+
+-- ArticleTitle (Title for an article)
+GetFieldValue("Transaction", "PhotoArticleTitle");
+
+-- JournalTitle (Title for a journal)
+GetFieldValue("Transaction", "PhotoJournalTitle");
+
+-- ISSN (ISSN or ISBN for a request)
+GetFieldValue("Transaction", "ISSN");
+
