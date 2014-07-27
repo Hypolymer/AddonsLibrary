17811d72c49bbcf2d6134e8e5cb6c0a50ce151d6
Hypolymer <jonesw@geneseo.edu>
2014-07-27 19:23:11 -0400

    Update AtlasHelpers.lua

--- AtlasHelpers.lua
+++ AtlasHelpers.lua
@@ -1,6 +1,23 @@
--- provide description
--- provide require
+-- Atlas helpers is installed by default with the ILLiad installation.
+-- Atlas helpers in both a URL encoder and a URL decoder.
+-- The function of the encoder is to write URL friendly strings (removing unicode characters)
+-- The function of the decoder is to read strings that have been URL encoded to display human readable text.
+-- Useful tool to encode and decode text on the web:  http://meyerweb.com/eric/tools/dencoder/
 
-local message = "<Author>Göhner, Dietmar</Author><Title>Beiträge zur kenntnis des südalpinen Juras mit besonderer berücksichtigung der unterjurassischen Lithiotis-Fazies </Title>"
+-- You must provide the following line before the function init() to load the Atlas Helpers class:
+
+require "Atlas.AtlasHelpers";
+
+-- Below is an example of a string containing unicode that cannot be passed through a WebClient:
+
+local message = "<Author>Göhner, Dietmar</Author>"
+
+
+-- In order to URL encode the string, write the following:
 
 AtlasHelpers.UrlEncode(message)
+
+
+-- This line will create the URL encoded text below:
+%3CAuthor%3EGo%CC%88hner%2C%20Dietmar%3C%2FAuthor
+
