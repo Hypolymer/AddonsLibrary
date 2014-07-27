f62c45399cd9845dae5eaa3ba09308003d663bf4
Hypolymer <jonesw@geneseo.edu>
2014-07-27 17:37:27 -0400

    Update AddNote.lua

--- AddNote.lua
+++ AddNote.lua
@@ -1,4 +1,7 @@
--- Add description
+-- This code grabs the current transaction number and then adds a note to the transaction.
+-- This note can be equal to a string of text, or to a pre-defined variable.
+-- Example:
+
 local currentTN = GetFieldValue("Transaction", "TransactionNumber");
 ExecuteCommand("AddNote", {currentTN, "NCIP Response for BorrowingCheckInItem received successfully"});
 SaveDataSource("Transaction");
