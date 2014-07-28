bac4b04e5c6339ffbb850a5fe518782517c52c43
Hypolymer <jonesw@geneseo.edu>
2014-07-27 21:22:30 -0400

    Update Addon Shell.lua

--- Addon Shell.lua
+++ Addon Shell.lua
@@ -19,32 +19,32 @@ AddonNameForm.RibbonPage = nil;
 
 
 function Init()
-		-- The line below makes this Addon only work on Loan Transactions.
-		if GetFieldValue("Transaction", "RequestType") == "Loan" then
-			interfaceMngr = GetInterfaceManager();
+    -- The line below makes this Addon only work on Loan Transactions.
+    if GetFieldValue("Transaction", "RequestType") == "Loan" then
+    interfaceMngr = GetInterfaceManager();
 			
-			-- Create browser
-			AddonNameForm.Form = interfaceMngr:CreateForm("AddonName", "Script");
-			AddonNameForm.Browser = BingForm.Form:CreateBrowser("AddonName", "AddonName", "AddonName");
+    -- Create browser
+    AddonNameForm.Form = interfaceMngr:CreateForm("AddonName", "Script");
+    AddonNameForm.Browser = BingForm.Form:CreateBrowser("AddonName", "AddonName", "AddonName");
 			
-			-- Hide the text label
-			AddonNameForm.Browser.TextVisible = false;
+    -- Hide the text label
+    AddonNameForm.Browser.TextVisible = false;
 			
-			--Suppress Javascript errors
-			AddonNameForm.Browser.WebBrowser.ScriptErrorsSuppressed = true;
+    --Suppress Javascript errors
+    AddonNameForm.Browser.WebBrowser.ScriptErrorsSuppressed = true;
 			
-			-- Since we didn't create a ribbon explicitly before creating our browser, it will have created one using the name we passed the CreateBrowser method.  We can retrieve that one and add our buttons to it.
-			AddonNameForm.RibbonPage = BingForm.Form:GetRibbonPage("AddonName");
-			-- The GetClientImage("Search32") pulls in the magnifying glass icon.  There are other icons that can be used.
-			AddonNameForm.RibbonPage:CreateButton("Search", GetClientImage("Search32"), "Search", "AddonName");
-            		AddonNameForm.Form:Show();
+    -- Since we didn't create a ribbon explicitly before creating our browser, it will have created one using the name we passed the CreateBrowser method.  We can retrieve that one and add our buttons to it.
+    AddonNameForm.RibbonPage = BingForm.Form:GetRibbonPage("AddonName");
+    -- The GetClientImage("Search32") pulls in the magnifying glass icon.  There are other icons that can be used.
+    AddonNameForm.RibbonPage:CreateButton("Search", GetClientImage("Search32"), "Search", "AddonName");
+    AddonNameForm.Form:Show();
             
-			if settings.autoSearch then
-				Search();
-			end
-		end
+	if settings.autoSearch then
+	    Search();
 	end
+    end
 end
+
 
 
 function Search()
