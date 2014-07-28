58b5f02f2be0dbca24d809018884d63c53911379
Hypolymer <jonesw@geneseo.edu>
2014-07-27 20:53:31 -0400

    Update PopupMessage.lua

--- PopupMessage.lua
+++ PopupMessage.lua
@@ -1,6 +1,14 @@
--- Include this line within a function to display informaiton in a text box
+-- Include this line within a function to display information in a text box.
 -- The first line in quotes is the message to the user.
--- The second line in quotes is the name/heading of the MessageBox
+-- The second line in quotes is the name/heading of the MessageBox.
 
+interfaceMngr:ShowMessage("First Line in Quotes - user message.", "Title of popup box");
 
-interfaceMngr:ShowMessage("ISxN is not available from request form", "Insufficient Information");
+function search()
+    local Author = GetFieldValue("Transaction", "Author");
+    if Author ~= "" then
+        interfaceMngr:ShowMessage("The author is" .. Author, "Name of the Author");
+    else
+        interfaceMngr:ShowMessage("The author name is not available from the request form", "Insufficient information");
+    end
+end
