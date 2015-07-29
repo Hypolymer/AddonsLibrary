14bea9e816e567c11d97a70a86697afde6b4ed87
Hypolymer <jonesw@geneseo.edu>
2015-07-29 15:30:15 -0400

    Create AddonName.lua

---
+++ FriendsURL2/AddonName.lua
@@ -0,0 +1,87 @@
+-- About AddonName.lua
+--
+-- Author: Bill Jones III, SUNY Geneseo, IDS Project, jonesw@geneseo.edu
+-- AddonName.lua provides a basic shell structure for an Addon.
+-- There is a config file that is associated with this Addon.
+-- scriptActive must be set to true for the script to run.
+-- autoSearch (boolean) determines whether the search is performed automatically when a request is opened or not.
+--
+-- set autoSearch to true for this script to automatically run the search when the request is opened.
+
+local settings = {};
+settings.autoSearch = GetSetting("AutoSearch");
+settings.FriendsURL = GetSetting("FriendsURL");
+settings.FriendsURL2 = GetSetting("FriendsURL2");
+local interfaceMngr = nil;
+local AddonNameForm = {};
+AddonNameForm.Form = nil;
+AddonNameForm.Browser = nil;
+AddonNameForm.RibbonPage = nil;
+
+function Init()
+    -- The line below makes this Addon only work on Loan Transactions.
+    -- if GetFieldValue("Transaction", "RequestType") == "Loan" then
+    interfaceMngr = GetInterfaceManager();
+
+    -- Create browser
+    AddonNameForm.Form = interfaceMngr:CreateForm("AddonName", "Script");
+    AddonNameForm.Browser = AddonNameForm.Form:CreateBrowser("AddonName", "AddonName", "AddonName");
+
+    -- Hide the text label
+    AddonNameForm.Browser.TextVisible = false;
+
+    --Suppress Javascript errors
+    AddonNameForm.Browser.WebBrowser.ScriptErrorsSuppressed = true;
+
+    -- Since we didn't create a ribbon explicitly before creating our browser, it will have created one using the name we passed the CreateBrowser method. We can retrieve that one and add our buttons to it.
+    AddonNameForm.RibbonPage = AddonNameForm.Form:GetRibbonPage("Google");
+	AddonNameForm.RibbonPage = AddonNameForm.Form:GetRibbonPage("AddonName");
+    -- The GetClientImage("Search32") pulls in the magnifying glass icon. There are other icons that can be used.
+    -- AddonNameForm.RibbonPage:CreateButton("Search", GetClientImage("Search32"), "Search", "AddonName");
+	-- Here we are adding a new button to the ribbon
+	AddonNameForm.RibbonPage:CreateButton("Search ISxN", GetClientImage("Search32"), "SearchISxN", "Google");
+	AddonNameForm.RibbonPage:CreateButton("Search Title", GetClientImage("Search32"), "SearchTitle", "Google");
+	AddonNameForm.RibbonPage:CreateButton("Phrase Search", GetClientImage("Search32"), "SearchPhrase", "Google");
+	AddonNameForm.RibbonPage:CreateButton("New URL", GetClientImage("Fraud32"), "NewURL", "AddonName");
+	AddonNameForm.RibbonPage:CreateButton("My Favorite Site", GetClientImage("Fraud32"), "NewURL2", "AddonName");
+
+    AddonNameForm.Form:Show();
+            
+if settings.autoSearch then
+SearchISxN();
+end
+end
+
+function NewURL()
+		AddonNameForm.Browser:Navigate(settings.FriendsURL);
+
+end
+
+function NewURL2()
+		AddonNameForm.Browser:Navigate(settings.FriendsURL2);
+
+end
+
+function SearchISxN()
+    if GetFieldValue("Transaction", "ISSN") ~= "" then
+		AddonNameForm.Browser:Navigate("https://www.google.com/?gws_rd=ssl#q=" .. GetFieldValue("Transaction", "ISSN"));
+	else
+       interfaceMngr:ShowMessage("ISxN is not available from request form", "Insufficient Information");
+	end
+end
+
+function SearchPhrase()
+    if GetFieldValue("Transaction", "RequestType") == "Loan" then  
+	   AddonNameForm.Browser:Navigate("https://www.google.com/?gws_rd=ssl#q=" ..  "\"" .. GetFieldValue("Transaction", "LoanTitle")  .. "\"");
+	else
+	   AddonNameForm.Browser:Navigate("https://www.google.com/?gws_rd=ssl#q=" ..  "\"" .. GetFieldValue("Transaction", "PhotoArticleTitle") .. "\"");
+	end
+end
+
+function SearchTitle()
+    if GetFieldValue("Transaction", "RequestType") == "Loan" then  
+	   AddonNameForm.Browser:Navigate("https://www.google.com/?gws_rd=ssl#q=" ..  GetFieldValue("Transaction", "LoanTitle"));
+	else
+	   AddonNameForm.Browser:Navigate("https://www.google.com/?gws_rd=ssl#q=" .. GetFieldValue("Transaction", "LoanTitle"));
+	end
+end
